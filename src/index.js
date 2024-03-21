4eb84b3cd636521678968a88b45b66f5f779e6b1
Hye Mee Park <hyemee1021@gmail.com>
2024-03-21 11:43:35 +1100

    initializing redux

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import store from "./redux/store";
+import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
